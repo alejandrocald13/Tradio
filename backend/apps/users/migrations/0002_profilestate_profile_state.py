3e2ce63a8805678bb9f80d9565b8e50b19d51b54
alejandrocald13 <robertoalejandrocalderon@gmail.com>
2025-09-21 20:13:04 -0600

    fix(users): fix migrations when creating new profiles

--- backend/apps/users/migrations/0002_profilestate_profile_state.py
+++ backend/apps/users/migrations/0002_profilestate_profile_state.py
@@ -23,6 +23,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='profile',
             name='state',
-            field=models.ForeignKey(default=apps.users.models.get_default_user_state, on_delete=django.db.models.deletion.PROTECT, related_name='profiles', to='users.profilestate'),
+            field=models.ForeignKey(default='', on_delete=django.db.models.deletion.PROTECT, related_name='profiles', to='users.profilestate'),
         ),
     ]
