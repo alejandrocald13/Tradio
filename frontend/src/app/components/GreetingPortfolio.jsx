0a58897968592e382789dc36794a09963b7865c5
Daniela Matul <mayda.matul@gmail.com>
2025-09-24 03:19:10 -0600

    refactor(portfolio): delete btn component to get doc

--- frontend/src/app/components/GreetingPortfolio.jsx
+++ frontend/src/app/components/GreetingPortfolio.jsx
@@ -7,15 +7,6 @@ export default function GreetingPortafolio(){
     // Datos que necesito aca
     // Nombre
     const name = 'Daniela'
-    const [getInforme, setGetInforme] = useState(false)
-    
-        const clickInforme = () => {
-            if (getInforme){
-                setGetInforme(false)
-            } else{
-                setGetInforme(true)
-            }
-        }
     return (
         <>
             <div className="greeting-main-container-portafolio">
@@ -24,26 +15,7 @@ export default function GreetingPortafolio(){
                     <p className="message-portafolio">Veamos cómo crece tu portafolio hoy.</p>
                 </div>
                 <div className="image-container-portafolio">
-                         <div className="getPDF">
-                            <button className="btn-get-pdf" onClick={clickInforme}>
-                                Enviar Informe
-                            </button>
-                                {getInforme &&(
-                                    <>
-                                        <p>Aqui </p>
-                                        <p>pediremos </p>
-                                        <p>fechas </p>
-
-
-                                    </>
-                                )}
-
-                        </div>
-                        <div className="historial-container">
-                            <a href="http://localhost:3000" className="historial-link">
-                                Ver Historial
-                            </a>
-                        </div>
+                        
                 </div>
             </div>
         </>
