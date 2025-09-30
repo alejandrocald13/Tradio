712b03ba0c82072489f47550c9bbc4d4d39da400
Juan <juanjluarca@gmail.com>
2025-09-30 00:22:29 -0600

    feat(backend-portfolio): modifying the model, adding buy/sell methods

--- backend/apps/portfolio/models.py
+++ backend/apps/portfolio/models.py
@@ -12,3 +12,16 @@ class Portfolio(models.Model):
     stock = models.ForeignKey(Stock, on_delete=models.PROTECT)
     quantity = models.FloatField(default=0)
     updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def buy(self, amount):
+        self.quantity += amount
+        self.is_active = True
+        self.save()
+
+    def sell(self, amount):
+        self.quantity -= amount
+        if self.quantity <= 0:
+            self.quantity = 0
+            self.is_active = False
+        self.save()
