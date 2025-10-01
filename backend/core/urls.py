a8c598b62026a6b3e6dc0dc626d8242ef961e676
alejandrocald13 <robertoalejandrocalderon@gmail.com>
2025-09-30 19:48:00 -0600

    fix(core): reordering endpoints from modules

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -21,24 +21,22 @@ from rest_framework_simplejwt.views import (TokenObtainPairView, TokenRefreshVie
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     # OpenAPI/Swagger
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+
     # Stocks
-    path('', include('apps.stock.urls')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-  
+    path('api/', include('apps.stock.urls')),
+
     # Users
     path('api/', include('apps.users.urls')),
 
-
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
     # Finance (Transactions)
     path('api/', include('apps.finance.urls')),
+
+
+    # Tokens
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    
 ]
