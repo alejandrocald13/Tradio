0b81866ca4066c3c24be1424a50129854d4a2f43
Juan <juanjluarca@gmail.com>
2025-09-30 18:36:52 -0600

    feat(backend-stock): implementing the price update via API

--- backend/apps/stock/api.py
+++ backend/apps/stock/api.py
@@ -1,8 +1,14 @@
-from rest_framework import viewsets, filters
+from rest_framework import viewsets, filters, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from rest_framework.permissions import IsAdminUser
+import requests
+import os
 from .models import Stock, Category
 from .serializers import StockSerializer, CategorySerializer
 from .permissions import IsAdminOrReadOnly
 
+API_KEY = os.getenv("FINNHUB_API_KEY")
 
 class StockViewSet(viewsets.ModelViewSet):
     queryset = Stock.objects.all() # para devolver únicamente stocks activos
@@ -15,6 +21,44 @@ class StockViewSet(viewsets.ModelViewSet):
 
     def perform_destroy(self, instance):
         instance.soft_delete()
+
+    @action(detail=False, methods=['post'], permission_classes=[IsAdminUser])
+    def update_prices(self, request):
+        activos = Stock.objects.filter(is_active=True)
+        resultados = []
+
+        for stock in activos:
+            symbol = stock.symbol
+            url = f"https://finnhub.io/api/v1/quote?symbol={symbol}&token={API_KEY}"
+            try:
+                response = requests.get(url)
+                data = response.json()
+                new_price = data.get('c')  # current price
+
+                if new_price:
+                    stock.current_price = new_price
+                    stock.save()
+                    resultados.append({
+                        "symbol": symbol,
+                        "price": new_price,
+                        "status": "updated"
+                    })
+                else:
+                    resultados.append({
+                        "symbol": symbol,
+                        "status": "no_price_returned"
+                    })
+
+            except Exception as e:
+                resultados.append({
+                    "symbol": symbol,
+                    "status": f"error: {e}"
+                })
+
+        return Response({
+            "message": "Update completed",
+            "results": resultados
+        }, status=status.HTTP_200_OK)
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
