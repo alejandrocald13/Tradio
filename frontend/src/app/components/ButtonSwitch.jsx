b3cfbb6a7cc880e060204829df099a0577c37d3a
Daniela Matul <mayda.matul@gmail.com>
2025-10-03 00:24:44 -0600

    feat(buttonSwitch): add component  button switch

---
+++ frontend/src/app/components/ButtonSwitch.jsx
@@ -0,0 +1,22 @@
+'useClient'
+
+import { useEffect, useState } from 'react'
+import '../styles/ButtonSwitch.css'
+import Modal from './Modal'
+
+
+
+// Datos que se necesitan enviar para que el btn funcione. 
+// id: (para diferenciar cada input y si label)
+// value: Estado del input:checkbox (true/false)
+// funcion que recibe el cambio del input (true/false)
+export default function ButtonSwitch ({id, value, getValue}) {
+    return (
+        <>
+            <div className="btn">
+                <input type="checkbox" id={id} checked={value} onChange={(e) => getValue({value: e.target.checked, id: id})}/>
+                <label htmlFor={id} className='lbl-switch'></label>
+            </div>
+        </>
+    )
+}
