22e2b28283609a94b89676890e6cd249e4f6aa30
José Ronquillo <114964339+JoseRonquillo@users.noreply.github.com>
2025-10-02 21:50:07 -0600

    Correcion espaciado

--- backend/apps/wallet/models.py
+++ backend/apps/wallet/models.py
@@ -19,6 +19,7 @@ class Movement(models.Model):
     total = models.DecimalField(max_digits=12, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
 
+
 class ReferalCode(models.Model):
     code = models.CharField(max_length=50, unique=True)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
