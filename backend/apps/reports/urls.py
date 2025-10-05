eb138052bcea3504ff22fc863203f95586028481
alejandrocald13 <robertoalejandrocalderon@gmail.com>
2025-10-04 23:37:02 -0600

    fix: endpoints names

--- backend/apps/reports/urls.py
+++ backend/apps/reports/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from apps.reports.views import ReportesEstadoView
 
 urlpatterns = [
-    path("state/", ReportesEstadoView.as_view(), name="reportes-estado"),
+    path("reports/", ReportesEstadoView.as_view(), name="reportes-estado"),
 ]
