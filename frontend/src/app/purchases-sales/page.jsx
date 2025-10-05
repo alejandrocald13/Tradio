46c54d24b118cc9770e926c629b32cd349464d14
Josué Bautista <josubauti@outlook.com>
2025-10-05 02:30:03 -0600

    fix: change the filters of date and the button to change the table

--- frontend/src/app/purchases-sales/page.jsx
+++ frontend/src/app/purchases-sales/page.jsx
@@ -66,15 +66,19 @@ export default function ComprasVentasPage() {
       <main className={styles.main}>
         <div className={styles.headerRow}>
           <h1 className={styles.title}>{tab === "compras" ? "Compras" : "Ventas"}</h1>
+        </div>
+        <div className={styles.contenedorFiltro}>
 
-          <div className={styles.headerControls}>
             <div className={styles.dateFilter}>
               <label className={styles.dateLabel}>Desde:</label>
               <input type="date" className={styles.dateInput} />
 
               <label className={styles.dateLabel}>Hasta:</label>
               <input type="date" className={styles.dateInput} />
-            </div>
+              <button>
+                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 24 24" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M10,18c1.846,0,3.543-0.635,4.897-1.688l4.396,4.396l1.414-1.414l-4.396-4.396C17.365,13.543,18,11.846,18,10 c0-4.411-3.589-8-8-8s-8,3.589-8,8S5.589,18,10,18z M10,4c3.309,0,6,2.691,6,6s-2.691,6-6,6s-6-2.691-6-6S6.691,4,10,4z"></path><path d="M11.412,8.586C11.791,8.966,12,9.468,12,10h2c0-1.065-0.416-2.069-1.174-2.828c-1.514-1.512-4.139-1.512-5.652,0 l1.412,1.416C9.346,7.83,10.656,7.832,11.412,8.586z"></path></svg>
+              </button>
+            </div>  
 
 
             <div className={styles.switcher}>
@@ -92,8 +96,6 @@ export default function ComprasVentasPage() {
               </button>
             </div>
 
-           
-          </div>
         </div>
 
         {/* Tabla de datos */}
