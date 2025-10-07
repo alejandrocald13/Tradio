54c5fa5a8a26641e9fc406983b35a48ef7292ef0
Josue Bautista <josubauti@outlook.com>
2025-10-06 23:12:51 -0600

    feat(actionAdminCard): add cards for actions, the basis for the final card

---
+++ frontend/src/app/components/ActionAdminCard.jsx
@@ -0,0 +1,16 @@
+import '../styles/ActionAdminCard.css'
+
+
+
+export default function ActionAdminCard({title, children }) {
+    return (
+        <>
+            <div className="cardActionAdmin">
+                {children}
+            </div>
+            <div>
+
+            </div>
+        </>
+    )
+}
