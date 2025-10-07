c79e5bbbffd225d5ec44e2abe02056fea7de602b
Daniela Matul <mayda.matul@gmail.com>
2025-10-06 23:18:55 -0600

    refactor(portfolio): modify styles and structure of portfolio

--- frontend/src/app/portfolio/page.jsx
+++ frontend/src/app/portfolio/page.jsx
@@ -107,10 +107,10 @@ export default function Portafolio (){
                     
                     <div className="aux-conteiner-p">
                         <div className="vertical-container-actions-portafolio">
-                            <div className="algo">
+                            <div className="btn-container">
 
                                 <button className="btn-get-date-email-portfolio" onClick={clickGetDate}>
-                                    <p className="btn-title">Informe Por Email</p>
+                                    <p className="btn-title">Send Email Report</p>
                                     <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M840.4 300H183.6c-19.7 0-30.7 20.8-18.5 35l328.4 380.8c9.4 10.9 27.5 10.9 37 0L858.9 335c12.2-14.2 1.2-35-18.5-35z"></path></svg>
                                 </button>
                             </div>
@@ -143,7 +143,7 @@ export default function Portafolio (){
                                 </div>
                             </div>)}
 
-                            <div className="algo">
+                            <div className="btn-container">
                                 <a href="/purchases-sales">
                                     <p className="btn-title">Historial</p>
                                     <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M715.8 493.5L335 165.1c-14.2-12.2-35-1.2-35 18.5v656.8c0 19.7 20.8 30.7 35 18.5l380.8-328.4c10.9-9.4 10.9-27.6 0-37z"></path></svg>
