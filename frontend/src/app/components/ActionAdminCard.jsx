5c6aa17c66d06bd7b6f649ae8c15573128d111f8
Josue Bautista <josubauti@outlook.com>
2025-10-12 12:29:18 -0600

    style(ActionAdminCard): styles for the title and the child

--- frontend/src/app/components/ActionAdminCard.jsx
+++ frontend/src/app/components/ActionAdminCard.jsx
@@ -5,11 +5,13 @@ import '../styles/ActionAdminCard.css'
 export default function ActionAdminCard({title, children }) {
     return (
         <>
-            <div className="cardActionAdmin">
-                {children}
-            </div>
-            <div>
-
+            <div className="action-card-admin">
+                <div className='title-action-card-admin'>
+                    <h3>{title}</h3>
+                </div>
+                <div className='children-action-card-admin'>
+                    {children}
+                </div>
             </div>
         </>
     )
