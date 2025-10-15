40f9b8943651d962cec7393b861db0dfff29f4f7
alejandrocald13 <robertoalejandrocalderon@gmail.com>
2025-10-14 20:57:13 -0600

    fix: fixing when importing endpoints

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,18 +1,25 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 from apps.users.views import (
-    RegisterView, LogoutView, MeView, UserViewSet, UserViewGetName
+    RegisterView, LogoutView, MeView, UserViewSet, UserViewGetName, DeleteSuperUser, SuperUserView
 )
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet, basename='users')
 
 urlpatterns = [
+    # users endpoints
+    
     path("auth/register", RegisterView.as_view(), name="auth-register"),
     path("auth/logout",   LogoutView.as_view(),   name="auth-logout"),
 
     path("auth/me",       MeView.as_view(),       name="auth-me"),
+    path("users/getname", UserViewGetName.as_view(), name="get-user-name"),
 
-    path("users/getname", UserViewGetName.as_view(), name="get-user-name")
+    # superusers endpoints
+    path("superusers/", SuperUserView.as_view(), name="get-super-users"),
+
+    path("superusers/<int:id>/delete", DeleteSuperUser.as_view(), name="disable-super-users"),
+
 ]
 urlpatterns += router.urls
