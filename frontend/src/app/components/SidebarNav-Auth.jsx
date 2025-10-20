cbc5282512671168e2a08d9d7eeeb190c60ec9f9
Josue Bautista <josubauti@outlook.com>
2025-10-20 04:23:39 -0600

    feat: Add the home page and profile to the sidebar to navigate between them

--- frontend/src/app/components/SidebarNav-Auth.jsx
+++ frontend/src/app/components/SidebarNav-Auth.jsx
@@ -11,10 +11,10 @@ export default function SidebarNav() {
   const pathname = usePathname();
 
   const items = [
-  // { id: "profile",   label: "Profile",         href: "/profile" },
+  { id: "authHome",  label: "Home",            href: "/authHome" },
+  { id: "profile",   label: "Profile",         href: "/profile" },
   { id: "wallet",    label: "Wallet",          href: "/wallet" },
   { id: "actions",   label: "Actions",         href: "/actions" },
-  // { id: "purchases", label: "Purchases/Sales", href: "/purchases-sales" },
   { id: "referrals", label: "Referrals",       href: "/referrals" },
   { id: "portfolio", label: "Portfolio",       href: "/portfolio" },
   ];
@@ -22,11 +22,31 @@ export default function SidebarNav() {
   const logoutItem = { id: "logout", label: "Logout", href: "/" };
 
   const DefaultIcons = {
+    authHome: (
+      <svg stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 1024 1024" height="1em" width="1em" xmlns="http://www.w3.org/2000/svg"><path d="M946.5 505L560.1 118.8l-25.9-25.9a31.5 31.5 0 0 0-44.4 0L77.5 505a63.9 63.9 0 0 0-18.8 46c.4 35.2 29.7 63.3 64.9 63.3h42.5V940h691.8V614.3h43.4c17.1 0 33.2-6.7 45.3-18.8a63.6 63.6 0 0 0 18.7-45.3c0-17-6.7-33.1-18.8-45.2zM568 868H456V664h112v204zm217.9-325.7V868H632V640c0-22.1-17.9-40-40-40H432c-22.1 0-40 17.9-40 40v228H238.1V542.3h-96l370-369.7 23.1 23.1L882 542.3h-96.1z"></path></svg>
+    ),
     profile: (
       <svg 
-      stroke="currentColor" fill="currentColor" strokeWidth="0" viewBox="0 0 16 16" height="1em" width="1em" 
-      xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 
-      100-6 3 3 0 000 6z" clipRule="evenodd"></path>
+        fill="currentColor" 
+        stroke="none" 
+        viewBox="0 0 24 24" 
+        height="1em" width="1em" 
+        xmlns="http://www.w3.org/2000/svg">
+        <path fillRule="evenodd" clipRule="evenodd" d="M16 9C16 
+          11.2091 14.2091 13 12 13C9.79086 13 8 11.2091 8 9C8 6.79086 
+          9.79086 5 12 5C14.2091 5 16 6.79086 16 9ZM14 9C14 10.1046 
+          13.1046 11 12 11C10.8954 11 10 10.1046 10 9C10 7.89543 10.8954 
+          7 12 7C13.1046 7 14 7.89543 14 9Z"></path>
+        <path fillRule="evenodd" clipRule="evenodd" d="M12 1C5.92487 
+          1 1 5.92487 1 12C1 18.0751 5.92487 23 12 23C18.0751 23 23 
+          18.0751 23 12C23 5.92487 18.0751 1 12 1ZM3 12C3 14.0902 
+          3.71255 16.014 4.90798 17.5417C6.55245 15.3889 9.14627 14 
+          12.0645 14C14.9448 14 17.5092 15.3531 19.1565 17.4583C20.313 
+          15.9443 21 14.0524 21 12C21 7.02944 16.9706 3 12 3C7.02944 
+          3 3 7.02944 3 12ZM12 21C9.84977 21 7.87565 20.2459 6.32767 
+          18.9878C7.59352 17.1812 9.69106 16 12.0645 16C14.4084 16 
+          16.4833 17.1521 17.7538 18.9209C16.1939 20.2191 14.1881 21 
+          12 21Z"></path>
       </svg>
     ),
     purchases: (
