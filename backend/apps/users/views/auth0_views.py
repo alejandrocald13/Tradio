3e9bdd3cd72fd0e1a40db042c4b6fe7c8317c7ec
alejandrocald13 <robertoalejandrocalderon@gmail.com>
2025-10-23 11:03:02 -0600

    chore: deleting unnecesseary dependecies

--- backend/apps/users/views/auth0_views.py
+++ backend/apps/users/views/auth0_views.py
@@ -1,6 +1,3 @@
-import jwt
-from datetime import datetime, timedelta
-from django.conf import settings
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -15,6 +12,7 @@ class Auth0LoginView(APIView):
     """
 
     def post(self, request):
+        
         serializer = Auth0UserLoginSerializer(data=request.data)
 
         if not serializer.is_valid():
