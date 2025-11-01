dece657f5328ac7b3fd1faba5810c3dbd82bbce4
alejandrocald13 <robertoalejandrocalderon@gmail.com>
2025-11-01 12:19:47 -0600

    fix: fixing and deleting is_open

--- backend/apps/transactions_all/market_clock/utils.py
+++ backend/apps/transactions_all/market_clock/utils.py
@@ -2,14 +2,14 @@ from datetime import datetime, time
 import pytz
 
 def is_open():
-    tz = pytz.timezone("America/Guatemala")
-    now = datetime.now(tz)
+    # tz = pytz.timezone("America/Guatemala")
+    # now = datetime.now(tz)
 
-    if now.weekday() in (5, 6):
-        return False
+    # if now.weekday() in (5, 6):
+    #     return False
 
-    open_time = time(8, 0)
-    close_time = time(20, 0) 
+    # open_time = time(8, 0)
+    # close_time = time(20, 0) 
 
     # return open_time <= now.time() <= close_time
     return True
